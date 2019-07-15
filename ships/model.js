b6bad9be3eaa0284b0910f8d1b945bfdb6e1cc37
vanyadelova <happy.vanya.delova@gmail.com>
2019-07-15 10:03:26 +0200

    cleaning code and formatting document

--- ships/model.js
+++ ships/model.js
@@ -1,34 +1,36 @@
-const Sequelize = require('sequelize')
-const sequelize = require('../db')
+const Sequelize = require("sequelize");
+const sequelize = require("../db");
 
-const Ship = sequelize.define('ships', {
+const Ship = sequelize.define(
+  "ships",
+  {
     name: {
       type: Sequelize.STRING,
       allowNull: false,
-      field: 'ship_name'
+      field: "ship_name"
     },
 
     uid: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        field: 'uid_name',
-      },
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      field: "uid_name"
+    },
 
-      speed: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        field: 'speed_name',
-      },
-      shipyardId: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        field: 'shipyard_id'
-      }
-    }, {
-        tableName: 'ships',
-        timestamps: false
-        
-      })
+    speed: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      field: "speed_name"
+    },
+    shipyardId: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      field: "shipyard_id"
+    }
+  },
+  {
+    tableName: "ships",
+    timestamps: false
+  }
+);
 
-      module.exports = Ship
-
+module.exports = Ship;
